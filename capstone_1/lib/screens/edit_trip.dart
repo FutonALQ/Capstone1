6ecb846dada34ec73472c74425d0c85055119f6e
FutonALQ <140398210+FutonALQ@users.noreply.github.com>
2023-12-12 16:55:11 +0300

    Update edit_trip.dart

--- capstone_1/lib/screens/edit_trip.dart
+++ capstone_1/lib/screens/edit_trip.dart
@@ -14,11 +14,10 @@ import 'package:image_picker/image_picker.dart';
 class EditTripScreen extends StatefulWidget {
   final Trip existingTrip;
 
-  const EditTripScreen({Key? key, required this.existingTrip})
-      : super(key: key);
+  const EditTripScreen({Key? key, required this.existingTrip});
 
   @override
-  _EditTripScreenState createState() => _EditTripScreenState();
+  State<EditTripScreen> createState() => _EditTripScreenState();
 }
 
 String selectedCategory = "sport";
@@ -32,9 +31,6 @@ class _EditTripScreenState extends State<EditTripScreen> {
   late TextEditingController dateController;
   late final ImagePicker picker;
   File? imageFile;
-
-
-
 
   late DateTime selectedDate;
   late TimeOfDay selectedTime;
@@ -90,7 +86,6 @@ class _EditTripScreenState extends State<EditTripScreen> {
     }
   }
 
-
   Future<void> _selectTime(BuildContext context) async {
     final TimeOfDay? picked = await showTimePicker(
       context: context,
@@ -110,15 +105,14 @@ class _EditTripScreenState extends State<EditTripScreen> {
   Future<void> getImage() async {
     XFile? image = await picker.pickImage(source: ImageSource.gallery);
 
+    Future getImage() async {
+      XFile? image = await picker.pickImage(source: ImageSource.gallery);
 
-  Future getImage() async {
-    XFile? image = await picker.pickImage(source: ImageSource.gallery);
-
-
-    if (image != null) {
-      setState(() {
-        imageFile = File(image.path);
-      });
+      if (image != null) {
+        setState(() {
+          imageFile = File(image.path);
+        });
+      }
     }
   }
 
@@ -137,76 +131,115 @@ class _EditTripScreenState extends State<EditTripScreen> {
         child: Center(
           child: SingleChildScrollView(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                GestureDetector(
-                  onTap: getImage,
-                  child: Center(
-                    child: CircleAvatar(
-
-                        backgroundColor: const Color(0xff8ECAE6),
-                        radius: 50,
-                        backgroundImage: widget.existingTrip.image != null
-                            ? NetworkImage(widget.existingTrip.image!)
-                            : NetworkImage(
-                                "https://t4.ftcdn.net/jpg/01/07/57/91/360_F_107579101_QVlTG43Fwg9Q6ggwF436MPIBTVpaKKtb.jpg")),
-
-
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  GestureDetector(
+                    onTap: getImage,
+                    child: Center(
+                      child: CircleAvatar(
+                          backgroundColor: const Color(0xff8ECAE6),
+                          radius: 50,
+                          backgroundImage: widget.existingTrip.image != null
+                              ? NetworkImage(widget.existingTrip.image!)
+                              : NetworkImage(
+                                  "https://t4.ftcdn.net/jpg/01/07/57/91/360_F_107579101_QVlTG43Fwg9Q6ggwF436MPIBTVpaKKtb.jpg")),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                const SizedBox(height: 16),
-                buildStyledTextField(
-                  controller: titleController,
-                  labelText: 'Trip Title',
-                ),
-                const SizedBox(height: 16),
-                InkWell(
-                  onTap: () {
-                    _selectDate(context);
-                  },
-                  child: IgnorePointer(
-                    child: buildStyledTextField(
-                      controller: dateController,
-                      labelText: 'Date',
-                      suffixIcon: const Icon(
-                        Icons.calendar_today,
-                        color: Color(0xff219EBC),
+                  const SizedBox(height: 16),
+                  const SizedBox(height: 16),
+                  buildStyledTextField(
+                    controller: titleController,
+                    labelText: 'Trip Title',
+                  ),
+                  const SizedBox(height: 16),
+                  InkWell(
+                    onTap: () {
+                      _selectDate(context);
+                    },
+                    child: IgnorePointer(
+                      child: buildStyledTextField(
+                        controller: dateController,
+                        labelText: 'Date',
+                        suffixIcon: const Icon(
+                          Icons.calendar_today,
+                          color: Color(0xff219EBC),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                InkWell(
-                  onTap: () {
-                    _selectTime(context);
-                  },
-                  child: IgnorePointer(
-                    child: buildStyledTextField(
-                      controller: timeController,
-                      labelText: 'Time',
-                      suffixIcon: const Icon(
-                        Icons.access_time,
-                        color: Color(0xff219EBC),
+                  const SizedBox(height: 16),
+                  InkWell(
+                    onTap: () {
+                      _selectTime(context);
+                    },
+                    child: IgnorePointer(
+                      child: buildStyledTextField(
+                        controller: timeController,
+                        labelText: 'Time',
+                        suffixIcon: const Icon(
+                          Icons.access_time,
+                          color: Color(0xff219EBC),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                DropdownButton<String>(
-                    value: selecteCity,
+                  const SizedBox(height: 16),
+                  DropdownButton<String>(
+                      value: selecteCity,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selecteCity = newValue!;
+                        });
+                      },
+                      items: <String>['Riyadh', 'Jeddah', 'Dammam']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                        );
+                      }).toList(),
+                      icon: const Icon(Icons.arrow_drop_down,
+                          color: Color(0xff8ECAE6)),
+                      iconSize: 30,
+                      elevation: 16,
+                      style: const TextStyle(color: Color(0xff023047)),
+                      underline: Container(
+                        height: 2,
+                        color: const Color(0xff8ECAE6),
+                      ),
+                      dropdownColor: const Color(0xff8ECAE6)),
+                  const SizedBox(height: 16),
+                  buildStyledTextField(
+                    controller: costController,
+                    labelText: 'Cost',
+                    keyboardType: TextInputType.number,
+                  ),
+                  const SizedBox(height: 16),
+                  buildStyledTextField(
+                    controller: descriptionController,
+                    labelText: 'Description',
+                    maxLines: 3,
+                  ),
+                  const SizedBox(height: 32),
+                  DropdownButton<String>(
+                    value: selectedCategory,
                     onChanged: (String? newValue) {
                       setState(() {
-                        selecteCity = newValue!;
+                        selectedCategory = newValue!;
                       });
                     },
-                    items: <String>['Riyadh', 'Jeddah', 'Dammam']
+                    items: <String>['sport', 'art', 'education', 'hangout']
                         .map<DropdownMenuItem<String>>((String value) {
                       return DropdownMenuItem<String>(
                         value: value,
                         child: Text(
                           value,
-                          style: const TextStyle(fontSize: 14),
+                          style: const TextStyle(
+                            fontSize: 14,
+                          ),
                         ),
                       );
                     }).toList(),
@@ -219,123 +252,80 @@ class _EditTripScreenState extends State<EditTripScreen> {
                       height: 2,
                       color: const Color(0xff8ECAE6),
                     ),
-                    dropdownColor: const Color(0xff8ECAE6)),
-                const SizedBox(height: 16),
-                buildStyledTextField(
-                  controller: costController,
-                  labelText: 'Cost',
-                  keyboardType: TextInputType.number,
-                ),
-                const SizedBox(height: 16),
-                buildStyledTextField(
-                  controller: descriptionController,
-                  labelText: 'Description',
-                  maxLines: 3,
-                ),
-                const SizedBox(height: 32),
-                DropdownButton<String>(
-                  value: selectedCategory,
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedCategory = newValue!;
-                    });
-                  },
-                  items: <String>['sport', 'art', 'education', 'hangout']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(
-                        value,
-                        style: const TextStyle(
-                          fontSize: 14,
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                  icon: const Icon(Icons.arrow_drop_down,
-                      color: Color(0xff8ECAE6)),
-                  iconSize: 30,
-                  elevation: 16,
-                  style: const TextStyle(color: Color(0xff023047)),
-                  underline: Container(
-                    height: 2,
-                    color: const Color(0xff8ECAE6),
+                    dropdownColor: const Color(0xff8ECAE6),
                   ),
-                  dropdownColor: const Color(0xff8ECAE6),
-                ),
-                const SizedBox(height: 30),
-                BlocConsumer<TripDetailsBloc, TripDetailsState>(
-                  listener: (context, state) {
-                    if (state is TripUpdateSuccessState) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                            content: Text('Trip updated successfully!',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black)),
-                            duration: Duration(seconds: 2),
-                            backgroundColor: Color(0xff8ECAE6)),
-                      );
-                    } else if (state is TripDetailsErrorState) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                            content: Text('Oops!  ${state.errorMessage}',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black)),
-                            duration: const Duration(seconds: 2),
-                            backgroundColor: Color(0xff8ECAE6)),
-                      );
-                    }
-                  },
-                  builder: (context, state) {
-                    return Container(
-                      width: 330,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: const Color(0xff8ECAE6),
-                      ),
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const AppNavigationBar()));
+                  const SizedBox(height: 30),
+                  BlocConsumer<TripDetailsBloc, TripDetailsState>(
+                    listener: (context, state) {
+                      if (state is TripUpdateSuccessState) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text('Trip updated successfully!',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black)),
+                              duration: Duration(seconds: 2),
+                              backgroundColor: Color(0xff8ECAE6)),
+                        );
+                      } else if (state is TripDetailsErrorState) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text('Oops!  ${state.errorMessage}',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black)),
+                              duration: const Duration(seconds: 2),
+                              backgroundColor: Color(0xff8ECAE6)),
+                        );
+                      }
+                    },
+                    builder: (context, state) {
+                      return Container(
+                        width: 330,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: const Color(0xff8ECAE6),
+                        ),
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const AppNavigationBar()));
 
-                          context.read<TripDetailsBloc>().add(
-                                UpdateTripEvent(
-                                  tripId: widget.existingTrip.id.toString(),
-                                  body: {
-                                    'title': titleController.text,
-                                    'time': timeController.text,
-                                    'date': dateController.text,
-                                    'category': selectedCategory,
-                                    'location': selecteCity,
-                                    'description': descriptionController.text,
-                                    'cost': int.parse(costController.text),
-                                    'creator_id': currentUser!.user_uuid,
-                                  },
-                                ),
-                              );
-                        },
-                        child: const Center(
-                          child: Text(
-                            'Edit Your trip',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Color(0xff023047),
-                              fontWeight: FontWeight.bold,
+                            context.read<TripDetailsBloc>().add(
+                                  UpdateTripEvent(
+                                    tripId: widget.existingTrip.id.toString(),
+                                    body: {
+                                      'title': titleController.text,
+                                      'time': timeController.text,
+                                      'date': dateController.text,
+                                      'category': selectedCategory,
+                                      'location': selecteCity,
+                                      'description': descriptionController.text,
+                                      'cost': int.parse(costController.text),
+                                      'creator_id': currentUser!.user_uuid,
+                                    },
+                                  ),
+                                );
+                          },
+                          child: const Center(
+                            child: Text(
+                              'Edit Your trip',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0xff023047),
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
+                      );
+                    },
+                  ),
+                ]),
           ),
         ),
       ),
